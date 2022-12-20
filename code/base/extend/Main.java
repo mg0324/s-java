9e49d430c971fb300d4e752b17e5b57be83a8772
mango mac <1092017732@qq.com>
2022-12-20 15:35:45 +0800

    面向对象 - 多态

--- code/base/extend/Main.java
+++ code/base/extend/Main.java
@@ -2,10 +2,10 @@ package code.base.extend;
 
 public class Main {
     public static void main(String[] args) {
-        Dog dog = new Dog("小黑",2);
+        Animal dog = new Dog("小黑",2);
         dog.cry();
-
-        Cat cat = new Cat("皮侃子",3);
+        
+        Animal cat = new Cat("皮侃子",3);
         cat.cry();
     }
 }
