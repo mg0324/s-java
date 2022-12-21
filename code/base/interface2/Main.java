057c2cffd7ff8521a8b2ba876a24b09d8a13c0de
mango mac <1092017732@qq.com>
2022-12-21 19:28:00 +0800

    完成Java基础 - 异常处理

--- code/base/interface2/Main.java
+++ code/base/interface2/Main.java
@@ -1,5 +1,8 @@
 package code.base.interface2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         // ChargeFace接口 来引用实现类对象变量
@@ -10,5 +13,21 @@ public class Main {
         Huawei huawei = new Huawei();
         huawei.type();
         huawei.value();
+        foo2();
+        
     }
+
+    public static <T extends Exception> void foo2() throws T {
+        System.out.println(111);
+        //int a = 1/0;
+        throw new ClassCastException("xx");
+    }
+
+    public class MyClass {
+        public int foo(List list) {return 1;}
+        public String foo(List list) {return "1";}
+    } 
+
+    
 }
+
