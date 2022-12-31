83846d805b84390a7b35aacbb00b7e88c2b5a1c2
mango mac <1092017732@qq.com>
2022-12-31 23:18:02 +0800

    完成jdbc部分

---
+++ code/jdbc/src/main/java/App.java
@@ -0,0 +1,6 @@
+package PACKAGE_NAME;/**
+ * @Author: mango
+ * @Date: 2022/12/31 7:58 PM
+ */
+public class App {
+}
---
+++ code/jdbc/src/main/java/DbUtilApp.java
@@ -0,0 +1,6 @@
+package PACKAGE_NAME;/**
+ * @Author: mango
+ * @Date: 2022/12/31 8:45 PM
+ */
+public class DbUtilApp {
+}
