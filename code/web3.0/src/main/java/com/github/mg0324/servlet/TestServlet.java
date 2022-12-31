747f563880a89ccacb7b5de5ac9d1d95fd9e07c8
mango mac <1092017732@qq.com>
2022-12-31 16:57:09 +0800

    完成jsp部分

--- code/web3.0/src/main/java/com/github/mg0324/servlet/TestServlet.java
+++ code/web3.0/src/main/java/com/github/mg0324/servlet/TestServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * @Author: mango
@@ -16,6 +17,7 @@ public class TestServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("hello HttpServlet!!!");
+        PrintWriter writer = resp.getWriter();
+        writer.println("hello HttpServlet!!!");
     }
 }
