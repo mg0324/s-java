40d8b76193d8b1753b5c6022fbdcd13f36af8c84
mango mac <1092017732@qq.com>
2023-02-24 18:33:37 +0800

    RPC专栏，新增并完成RPC的网络通信章节

--- code/netty-test/src/main/java/org/example/Main.java
+++ code/netty-test/src/main/java/org/example/Main.java
@@ -1,5 +1,31 @@
-package org.example;public class Main {
+package org.example;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.buffer.CompositeByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.DefaultFileRegion;
+import io.netty.channel.FileRegion;
+
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        ByteBuf buf = Unpooled.buffer();
+        buf.writeBytes("hello netty!!!".getBytes(StandardCharsets.UTF_8));
+        ByteBuf buf1 = buf.slice(1, 10);
+        System.out.println(buf1.toString());
+        //Unpooled.wrappedBuffer(byte[])
+        CompositeByteBuf compositeByteBuf = new CompositeByteBuf(ByteBufAllocator.DEFAULT,true,8);
+        compositeByteBuf.addComponent(buf);
+        compositeByteBuf.addComponent(buf1);
+        System.out.println(compositeByteBuf.toString());
+       
+        FileRegion fileRegion = new DefaultFileRegion();
+        fileRegion.transferTo()
     }
 }
