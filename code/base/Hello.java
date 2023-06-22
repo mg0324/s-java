167cd5e096c76a8b2181d06d5b1f5abcd1bc33eb
mango mac <1092017732@qq.com>
2023-06-22 10:21:09 +0800

    代码迁移到java-code仓库

--- code/base/Hello.java
+++
@@ -1,6 +0,0 @@
-package code.base;
-public class Hello{
-    public static void main(String[] args) {
-        System.out.println("hello world");
-    }
-}
--- code/plugin/plugin-api/src/main/java/com/github/mg0324/plugin/Plugin.java
+++
@@ -1,6 +0,0 @@
-package com.github.mg0324.plugin;
-
-public interface Plugin {
-    void start();
-    void stop();
-}
